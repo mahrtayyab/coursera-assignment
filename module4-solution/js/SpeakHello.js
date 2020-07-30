9b9a20d172d898a26300cae109ac16fd6e2ec798
Tayyab Kharl <tayyabmahr@gmail.com>
2020-07-30 11:19:06 +0500

    Update SpeakHello.js

--- module4-solution/js/SpeakHello.js
+++ module4-solution/js/SpeakHello.js
@@ -1,1 +1,11 @@
-//placeholder
+(function (window){
+	var helloSpeaker={};
+ helloSpeaker.speakWord = "Hello";
+
+helloSpeaker.speak= function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+window.helloSpeaker=helloSpeaker;
+
+
+})(window);
