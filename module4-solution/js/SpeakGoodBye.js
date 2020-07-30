2b0e0c8d943fd92fd79c66a7df39f9b11feba775
Tayyab Kharl <tayyabmahr@gmail.com>
2020-07-30 11:19:30 +0500

    Update SpeakGoodBye.js

--- module4-solution/js/SpeakGoodBye.js
+++ module4-solution/js/SpeakGoodBye.js
@@ -1,1 +1,12 @@
-//placeholder
+(function (window){
+	var byeSpeaker={};
+byeSpeaker.speakWord = "Good Bye";
+
+
+byeSpeaker.speak= function(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+window.byeSpeaker=byeSpeaker;
+
+}
+)(window);
